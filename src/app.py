0b1ed52afee3fd9ae936faf8eaea54353a7da68e
Nathan Bailey <dev@dnbailey.org>
2022-08-29 13:12:19 -0400

    Add features

--- src/app.py
+++ src/app.py
@@ -1,31 +1,53 @@
-from flask import Flask, jsonify
-import csv
+from flask import Flask, jsonify, request
+from utils import parse, require_apikey
+import logging
 
-def find_person(name):
-    with open("data.csv", newline="") as file:
-        data = csv.DictReader(file)
+logging.basicConfig(filename="app.log", filemode="w",
+                    format="%(name)s - %(levelname)s - %(message)s")
+
+data = parse("data.csv")
+
+
+def find_person(name, type):
+    if type != "f" and type != 'l':
+        logging.info("User did not specify f or l")
+    else:
         for line in data:
-            if name.lower() == line['fname'].lower():
+            if name.lower() == line[type + 'name'].lower():
                 return line
-        return "Not found"
+    return "Not found"
+
+
+def find_state(state):
+    for line in data:
+        if state.lower() == line['state'].lower():
+            return line
+    return "Not found"
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return jsonify({"response": "It works!"})
 
+
 @app.route('/user/')
 def user():
     return jsonify("No user defined."), 404
 
+
 @app.route('/user/<user_name>')
+@require_apikey
 def get_user(user_name):
-    user_data = find_person(user_name)
+    f_or_l = request.args.get("type")
+    user_data = find_person(user_name, f_or_l)
     if user_data == "Not found":
         return jsonify(f"{user_name} not found"), 404
 
     return jsonify({"firstName": user_data['fname'], "last_name": user_data['lname']})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ src/utils.py
@@ -0,0 +1,22 @@
+from flask import request, abort
+import csv
+
+
+def parse(filename):
+    with open(filename, newline="") as file:
+        data = csv.DictReader(file)
+        return list(data)
+
+
+def require_apikey(route_function):
+    def decorated_function(*args, **kwargs):
+        headers = request.headers
+        if headers.get("X-Api-Key") and headers.get("X-Api-Key") == "123":
+            return route_function(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated_function
+
+
+if __name__ == "__main__":
+    print("This should not be ran standalone.")
